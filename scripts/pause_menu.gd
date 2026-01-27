23829da2493a02ee4da797767214d7d9508f66cf
cynthiawinda <cynthiawindaw@gmail.com>
2026-01-27 15:09:07 +0700

    feat: pause menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@onready var pause_menu = $PauseMenu
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	pause_menu.visible = false
+
+func _on_pause_button_pressed() -> void:
+	get_tree().paused = true
+	pause_menu.visible = true
+
+func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
+	pause_menu.visible = false
+	
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
