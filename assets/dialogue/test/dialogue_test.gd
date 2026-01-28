c6500d20b20c22534902d873c723f81cb6a9608b
Medcro <muhammaddhiyarafi@gmail.com>
2026-01-28 23:11:39 +0700

    feat: setting up for the dialogue story

--- assets/dialogue/test/dialogue_test.gd
+++ assets/dialogue/test/dialogue_test.gd
@@ -3,14 +3,16 @@ class_name dialogue
 
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
+@export var num_day: String = "2"
 
 @onready var bg_sprite: TextureRect = $Background
+@onready var flashback_ui: ColorRect = %FlashbackEffect
 
 func _ready() -> void:
 	start_story()
 
 func set_bg(img_name: String) -> void:
-	var new_tex = load("res://assets/levels/test/" + img_name + ".png")
+	var new_tex = load("res://assets/levels/day " + num_day + "/" + img_name + ".png")
 	
 	var tween = create_tween()
 	# Fade out current bg
@@ -19,6 +21,14 @@ func set_bg(img_name: String) -> void:
 	tween.tween_callback(func(): bg_sprite.texture = new_tex)
 	tween.tween_property(bg_sprite, "modulate:a", 1.0, 0.5)
 
+func set_flashback(active: bool) -> void:
+	var tween = create_tween()
+	if active:
+		# Change color to Sepia or Light Gray
+		flashback_ui.color = Color(0.7, 0.6, 0.4, 0.3) # Vintage Sepia
+		tween.tween_property(flashback_ui, "self_modulate:a", 1.0, 1.0)
+	else:
+		tween.tween_property(flashback_ui, "self_modulate:a", 0.0, 1.0)
+
 func start_story():
-	var resource = load("res://assets/dialogue/test/test.dialogue")
-	DialogueManager.show_dialogue_balloon(resource, "start", [self])
+	DialogueManager.show_dialogue_balloon(dialogue_resource, "start", [self])
